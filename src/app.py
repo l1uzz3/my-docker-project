d0b77789de11e0e3773ea07f66857b13a54af7e7
l1uzz3 <stefan.liute96@e-uvt.ro>
2024-10-22 20:49:12 +0300

    modified app.py. tested it to see if it worked on localhost when deployed.

--- src/app.py
+++ src/app.py
@@ -1,2 +1,11 @@
-print("Hello, Docker!")
-print("Successfully installed VScode from Ubuntu.\nNow running from WSL:Ubuntu")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Printing this from the docker container!"
+
+if __name__ == "__main__":
+    # This is where you specify the host and port
+    app.run(port=8080)
